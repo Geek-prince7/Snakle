0ea5b668f5b49d82377bf3401515856b809491c4
Prince <vibpri729@gmail.com>
2022-04-22 21:09:39 +0530

    added apple+ apple random movement +snake size auto increase +added game over

--- main.py
+++ main.py
@@ -1,53 +1,43 @@
+# Game over scenario and pausing a game
+
 import pygame
 from pygame.locals import *
 import time
 import random
 
+SIZE = 40
+BACKGROUND_COLOR = (110, 110, 5)
 
 class Apple:
     def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
         self.image = pygame.image.load("Resouce/apple.jpg").convert()
-        self.surface = parent_screen
         self.x = 120
-        self.y = 160
+        self.y = 120
 
     def draw(self):
-        self.surface.blit(self.image, (self.x, self.y))
+        self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
     def move(self):
-        self.x = random.randint(1,25)*40
-        self.y = random.randint(1,16)*40
-
-    def return_apple_pos(self):
-        return self.x , self.y
-
+        self.x = random.randint(1,24)*SIZE
+        self.y = random.randint(1,17)*SIZE
 
 class Snake:
-    def __init__(self, parent_screen, length):
-        self.surface = parent_screen
-        self.block = pygame.image.load("Resouce/block.jpg").convert()
-        self.b_height = self.block.get_height()
-        self.b_width = self.block.get_width()
-        self.len = length
-        self.x = [40] * length
-        self.y = [40] * length
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("Resouce/block.jpg").convert()
         self.direction = 'down'
 
-    def draw(self, len1=1):
-        self.surface.fill((110, 110, 5))
-        newl = int(len1)
-        if (self.len < newl):
-            self.len = newl
-            self.x.append(self.x[0])
-            self.y.append(self.y[0])
+        self.length = 1
+        self.x = [40]
+        self.y = [40]
 
-        for i in range(self.len):
-            self.surface.blit(self.block, (self.x[i], self.y[i]))
-        pygame.display.flip()
+    def move_left(self):
+        self.direction = 'left'
 
-    def return_snake_pos(self):
-        return self.x[0] , self.y[0]
+    def move_right(self):
+        self.direction = 'right'
 
     def move_up(self):
         self.direction = 'up'
@@ -55,100 +45,129 @@ class Snake:
     def move_down(self):
         self.direction = 'down'
 
-    def move_right(self):
-        self.direction = 'right'
-
-    def move_left(self):
-        self.direction = 'left'
-
     def walk(self):
+        # update body
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
 
-        for i in range(self.len - 1, 0, -1):
-            self.x[i] = self.x[i - 1]
-            self.y[i] = self.y[i - 1]
+        # update head
+        if self.direction == 'left':
+            self.x[0] -= SIZE
+        if self.direction == 'right':
+            self.x[0] += SIZE
         if self.direction == 'up':
-            self.y[0] -= self.b_height
-        elif self.direction == 'down':
-            self.y[0] += self.b_height
-        elif self.direction == 'right':
-            self.x[0] += self.b_width
-        elif self.direction == 'left':
-            self.x[0] -= self.b_width
+            self.y[0] -= SIZE
+        if self.direction == 'down':
+            self.y[0] += SIZE
+
         self.draw()
 
+    def draw(self):
+        self.parent_screen.fill(BACKGROUND_COLOR)
+
+        for i in range(self.length):
+            self.parent_screen.blit(self.image, (self.x[i], self.y[i]))
+        pygame.display.flip()
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
 
 class Game:
-    # constructor
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1200, 700))
-        self.length = 1
-        self.snake = Snake(self.surface, self.length)
+        pygame.display.set_caption("Codebasics Snake And Apple Game")
+        self.surface = pygame.display.set_mode((1000, 700))
+        self.snake = Snake(self.surface)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def reset(self):
+        self.snake = Snake(self.surface)
+        self.apple = Apple(self.surface)
+
+
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+        return False
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
-        self.collision()
-        self.score()
-        self.game_over()
+        self.display_score()
+        pygame.display.flip()
 
-
-    def size_change(self):
-        self.length += 1
-        self.snake.draw(self.length)
-
-
-
-    def collision(self):
-        if self.apple.return_apple_pos() == self.snake.return_snake_pos():
+        # snake eating apple scenario
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
             self.apple.move()
-            self.size_change()
 
-    def game_over(self):
-        for i in range(3,self.snake.len):
-            x1=self.snake.x[0]
-            y1=self.snake.y[0]
-            x2=self.snake.x[i]
-            y2=self.snake.y[i]
-            if x1 >= x2 and x1 < x2 + 40:
-                if y1 >= y2 and y1 < y2 + 40:
-                    print('go')
+        # snake colliding with itself
+        for i in range(2, self.snake.length):
+            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                raise "Collision Occured"
 
-    def score(self):
-        font=pygame.font.SysFont('arial',30)
-        scores=font.render(f"score : {self.snake.len*40 }",True,(255, 255, 255))
-        self.surface.blit(scores,(1000,10))
+    def display_score(self):
+        font = pygame.font.SysFont('arial',30)
+        score = font.render(f"Score: {self.snake.length}",True,(200,200,200))
+        self.surface.blit(score,(850,10))
+
+    def show_game_over(self):
+        self.surface.fill(BACKGROUND_COLOR)
+        font = pygame.font.SysFont('arial', 30)
+        line1 = font.render(f"Game is over! Your score is {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(line1, (200, 300))
+        line2 = font.render("To play again press Enter. To exit press Escape!", True, (255, 255, 255))
+        self.surface.blit(line2, (200, 350))
+
         pygame.display.flip()
 
     def run(self):
         running = True
+        pause = False
 
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
-                    if event.key == K_LEFT:
-                        self.snake.move_left()
 
-                    if event.key == K_RIGHT:
-                        self.snake.move_right()
+                    if event.key == K_RETURN:
+                        pause = False
 
-                    if event.key == K_UP:
-                        self.snake.move_up()
+                    if not pause:
+                        if event.key == K_LEFT:
+                            self.snake.move_left()
 
-                    if event.key == K_DOWN:
-                        self.snake.move_down()
+                        if event.key == K_RIGHT:
+                            self.snake.move_right()
+
+                        if event.key == K_UP:
+                            self.snake.move_up()
+
+                        if event.key == K_DOWN:
+                            self.snake.move_down()
 
                 elif event.type == QUIT:
                     running = False
-            self.play()
-            time.sleep(0.3)
+            try:
 
+                if not pause:
+                    self.play()
+
+            except Exception as e:
+                self.show_game_over()
+                pause = True
+                self.reset()
+
+            time.sleep(.25)
 
 if __name__ == '__main__':
     game = Game()
     game.run()
+
